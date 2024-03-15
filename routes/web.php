855693637af1969fbf3b099459cb9884ba0e5bdf
bachb2014728 <bachb2014728@student.ctu.edu.vn>
2024-03-15 16:40:16 +0700

    Cấu hình Router

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\auth\CartController;
+use App\Http\Controllers\auth\CategoryAuthController;
+use App\Http\Controllers\auth\HomeController;
+use App\Http\Controllers\auth\OrderAuthController;
+use App\Http\Controllers\auth\ProductAuthController;
+use App\Http\Controllers\auth\ProfileUserController;
+use App\Http\Controllers\auth\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MemberController;
@@ -59,4 +66,51 @@ Route::prefix('admin')->middleware('admin')->group(function(){
         Route::put('/', [ProfileController::class, 'updatePassword'])->name('password.update');
     });
 });
+
+// User
+Route::get('/login',[UserController::class,'login'])->name('login');
+Route::post('/login',[UserController::class,'postLogin'])->name('user.postLogin');
+Route::get('/register',[UserController::class,'register'])->name('register');
+Route::post('/register',[UserController::class,'postRegister'])->name('user.postRegister');
+Route::get('/logout',[UserController::class,'logout'])->name('logout');
+
+Route::get('/',[HomeController::class,'index'])->name('index');
+
+Route::group(['prefix'=> 'product'], function () {
+    Route::get('/',[ProductAuthController::class,'index'] )->name('sanpham.index');
+    Route::get('/{product}',[ProductAuthController::class,'show'])->name('sanpham.show');
+    Route::post('/rating/{product}',[ProductAuthController::class,'rateProduct'])->name('sanpham.rating');
+    Route::post('/post-comment/{product}',[ProductAuthController::class,'postComment'])->name('sanpham.comment');
+});
+Route::group(['prefix'=> 'cart'], function () {
+    Route::get('/',[CartController::class,'cart'])->name('cart.index');
+    Route::get('/cart-add', [CartController::class, 'addToCart'])->name('cart.addto');
+    Route::get('/cart-add/{id}', [CartController::class, 'add'])->name('cart.add');
+    Route::get('/cart-update-up/{id}', [CartController::class,'updateCartUp'])->name('cart.updateUp');
+    Route::get('/cart-update-down/{id}', [CartController::class,'updateCartDown'])->name('cart.updateDown');
+    Route::get('/cart-remove/{id}', [CartController::class, 'removeCart'])->name('cart.remove');
+    Route::get('/cart-clear', [CartController::class, 'clearCart'])->name('cart.clear');
+});
+Route::group(['prefix'=> 'order'], function () {
+    Route::get('/',[OrderAuthController::class,'index'])->name('order.home');
+    Route::get('/form', [OrderAuthController::class,'form'])->name('order.list');
+    Route::post('/form', [OrderAuthController::class,'submit_form'])->name('order.complete');
+    Route::get('/message-order/{id}', [OrderAuthController::class,'message_order'])->name('order.show');
+    Route::get('/edit-order/{id}', [OrderAuthController::class,'edit_order'])->name('order.edit');
+    Route::post('/update-order/{id}', [OrderAuthController::class,'update_order'])->name('order.update');
+    Route::get('/buy-quick/{url}', [OrderAuthController::class,'buy_quick'])->name('order.quick');
+    Route::post('/buy-quick/{url}', [OrderAuthController::class,'accept_buy_quick'])->name('order.acceptQuick');
+});
+Route::group(['prefix'=> 'profile-user'], function () {
+    Route::get('/', [ProfileUserController::class,'index'])->name('profile.indexUser');
+    Route::post('/update', [ProfileUserController::class,'update'])->name('profile.updateTo');
+    Route::post('/change-password', [ProfileUserController::class,'change_password'])->name('profile.password');
+    Route::post('/add-address',[ProfileUserController::class,'add_address'])->name('profile.moreAddress');
+    Route::delete('/delete-address/{id}', [ProfileUserController::class,'delete_address'])->name('profile.deleteAddress');
+    Route::post('/update-address/{id}', [ProfileUserController::class,'update_address'])->name('profile.updateAddress');
+    Route::post('/remove-order/{id}', [ProfileUserController::class,'remove_order'])->name('profile.removeOrder');
+    Route::get('/delete-user', [ProfileUserController::class,'delete_user'])->name('profile.delete');
+});
+Route::get('/cate/{slug}',[CategoryAuthController::class,'index'])->name('cate.index');
+Route::get('/cate/{slug}/{slug_E}',[CategoryAuthController::class,'cate_show'])->name('cate.show');
 
